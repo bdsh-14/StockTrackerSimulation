ab4a7412cc466f6a2042e9107e1971a696bc1e4f
Bidisha Biswas <bdsh.14@gmail.com>
2025-09-27 12:44:18 -0600

    Initial Commit

---
+++ StockTrackDummy/StockTrackDummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTrackDummyApp.swift
+//  StockTrackDummy
+//
+//  Created by Bidisha Biswas on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct StockTrackDummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
